12d307d125ecae71b889eeb7016dc69eac56d430
EliteProTech <proelite455@gmail.com>
2026-08-11 23:01:21 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,10 @@ export default {
     prefix: ['.'],
     botMode: 'self',
     botMessage: {
-        owner: 'This feature is for the Owner only.'
+        owner: 'This feature is for the Owner only.',
+        admin: 'This feature is for group Admins only.',
+        group: 'This command can only be used in a group.',
+        private: 'This command can only be used in a private chat.',
+        isBotAdmin: 'I need to be an admin in this group to do that.'
     }
 }
