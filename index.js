fda98ef6c88fddaa2228077906f027079383fb46
EliteProTech <proelite455@gmail.com>
2026-08-11 16:05:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -146,7 +146,7 @@ export default async function handleMessage(EliteProTech, m) {
         m.text = body
         m.isButtonResponse = isButtonResponse
 
-        const owner = readJSON('./database/owner.json')
+        const owner = readJSON('./lib/database/owner.json')
         const number = m.sender.split('@')[0]
         const botNumber = EliteProTech.decodeJid(EliteProTech.user.id).split('@')[0]
         m.isOwner = owner.includes(number) || number === botNumber
