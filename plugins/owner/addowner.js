6d7a3d30a1a4c5a0be257f99d631b0ea73d96522
EliteProTech <proelite455@gmail.com>
2026-08-15 02:12:50 +0100

    Create addowner.js

---
+++ plugins/owner/addowner.js
@@ -0,0 +1,51 @@
+import fs from 'fs'
+import path from 'path'
+
+const OWNER_DB_PATH = path.join(process.cwd(), 'lib', 'database', 'owner.json')
+
+function readOwners() {
+    try {
+        return JSON.parse(fs.readFileSync(OWNER_DB_PATH, 'utf-8'))
+    } catch {
+        return []
+    }
+}
+function writeOwners(owners) {
+    fs.writeFileSync(OWNER_DB_PATH, JSON.stringify(owners, null, 2))
+}
+function extractNumber(m, args) {
+    if (args[0]) {
+        const digits = args[0].replace(/[^0-9]/g, '')
+        if (digits) return digits
+    }
+    if (m.mentionedJid?.length) {
+        return m.mentionedJid[0].split('@')[0]
+    }
+    if (m.quoted?.sender) {
+        return m.quoted.sender.split('@')[0]
+    }
+    return null
+}
+
+let handler = async (m, { args }) => {
+    const number = extractNumber(m, args)
+    if (!number) {
+        return await m.reply('Provide a number, mention a user, or reply to their message.\nUsage: .addowner 234xxxxxxxxxx')
+    }
+    if (number === String(global.owner)) {
+        return await m.reply('That number is already the primary owner.')
+    }
+    const owners = readOwners()
+    if (owners.includes(number)) {
+        return await m.reply(`${number} is already an owner.`)
+    }
+    owners.push(number)
+    writeOwners(owners)
+
+    await m.reply(`Added ${number} as an owner.`)
+}
+
+handler.command = ['addowner']
+handler.owner = true
+
+export default handler
