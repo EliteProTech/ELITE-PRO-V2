2eafe4bc93102b55c3313a1c1068515d82025163
EliteProTech <proelite455@gmail.com>
2026-08-16 12:12:17 +0100

    Update config.js by removing stickerPack
    Removed stickerPack configuration and added autoLikeStatus.

--- config.js
+++ config.js
@@ -8,11 +8,7 @@ global.botName = 'ELITE-PRO-V2'
 global.prefix = '.'
 global.botMode = 'self'
 global.autoViewStatus = false
-
-global.stickerPack = {
-    packname: 'ELITE-PRO-V2',
-    author: 'EliteProTech'
-}
+global.autoLikeStatus = false
 global.botMessage = {
     owner: 'This feature is for the Owner only.',
     admin: 'This feature is for group Admins only.',
