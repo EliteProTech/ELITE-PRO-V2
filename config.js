40d73a2919ccdc3ff108301c69be82c500224917
EliteProTech <proelite455@gmail.com>
2026-08-29 17:11:01 +0100

    Update config.js

--- config.js
+++ config.js
@@ -15,7 +15,8 @@ global.botMessage = {
     admin: 'This feature is for group Admins only.',
     group: 'This command can only be used in a group.',
     private: 'This command can only be used in a private chat.',
-    isBotAdmin: 'I need to be an admin in this group to do that.'
+    isBotAdmin: 'I need to be an admin in this group to do that.',
+    connect: 'Bot connected successfully.'
 }
 global.stickerPack = {
     packname: 'ELITE-PRO-V2',
