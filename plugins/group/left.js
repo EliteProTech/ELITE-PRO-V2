b191bd68bc4befb070fddffea8be3896e260e398
EliteProTech <proelite455@gmail.com>
2026-09-02 12:33:07 +0100

    Restrict leave command to group owners only

--- plugins/group/left.js
+++ plugins/group/left.js
@@ -1,11 +1,9 @@
 let handler = async (m, { EliteProTech }) => {
-    if (!m.isGroup) return m.reply('This command can only be used in groups.')
     await EliteProTech.groupLeave(m.chat)
 }
 
 handler.command = ['left', 'leavegroup']
 handler.group = true
-handler.admin = true
-handler.isBotAdmin = true
+handler.owner = true
 
 export default handler
